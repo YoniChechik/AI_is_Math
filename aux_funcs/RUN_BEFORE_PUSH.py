a6cce3d8d84fc688cd31373d9ed16f7621a1ba5b
Jonathan Chechik <yonich@visionmap.co.il>
2019-09-12 16:48:32 +0300

    WIP automate site

--- aux_funcs/RUN_BEFORE_PUSH.py
+++ aux_funcs/RUN_BEFORE_PUSH.py
@@ -5,21 +5,25 @@ import os
 DO_PPT = 1
 DO_PY = 0
 
-
+#get all git cass dirs - starting with "p_"
 cwd = os.getcwd()
-git_dirs = os.path.join(cwd,"..")
+git_main_dirs_cwd = os.path.join(cwd,"..")
+git_class_subdirs = [x for x in os.listdir(git_main_dirs_cwd) if ( os.path.isdir(os.path.join(git_main_dirs_cwd,x)) and x.startswith("p_") ) ]
 
-git_class_subdirs = [x for x in os.listdir(git_dirs) if ( os.path.isdir(os.path.join(git_dirs,x)) and x.startswith("p_") ) ]
 for dir_name in git_class_subdirs:
-    fp = os.path.join(git_dirs,dir_name)
+    fp = os.path.join(git_main_dirs_cwd,dir_name)
 
     print("===== IN DIR: " + fp + " =====")
     for fn in os.listdir(fp):
 
         fn_no_ext = fn.split(".")[0]
+
+        # make .pptx -> .pdf
         if fn.endswith(".pptx") and DO_PPT:
             print("converting pptx: "+ fn)
             PPTtoPDF(os.path.join(fp,fn),os.path.join(fp,fn_no_ext+".pdf"))
+
+        # make .py -> .ipynb and compile ipynb
         elif fn.endswith(".py") and DO_PY:
             out_ipynb = os.path.join(fp,fn_no_ext+".ipynb")
 
---
+++ test.md
@@ -0,0 +1,4 @@
+tetegs
+
+<a href="https://nbviewer.jupyter.org/github/YoniChechik/AI_is_Math/blob/master/p_05_image_formation/Image%20formation.pdf#page=11">Go to the heading with this html anchor</a>
+

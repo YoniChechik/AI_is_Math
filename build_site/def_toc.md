5f7d886cbe436683cdab35b995e0faf12393ec0b
YoniChechik <chechik.yoni@gmail.com>
2019-09-21 23:42:42 +0300

    new files generation

--- build_site/def_toc.md
+++
@@ -1,8 +0,0 @@
----
-title: var_tit
-bigimg: var_class_page_path/var_bigimg_name
----
-
-## **var_class_name** [(slides)](var_class_page_path/slides/)
-
-var_raw_toc
--- build_site/file_converter.py
+++ build_site/file_converter.py
@@ -2,33 +2,59 @@ from py2ipynb import py2ipynb
 from ppt2pdf import PPTtoPDF
 import os
 
-DO_PPT = 0
-DO_PY = 1
 
-#get all git cass dirs - starting with "p_"
-cwd = os.getcwd()
-git_main_dirs_cwd = os.path.join(cwd,"..")
-git_class_subdirs = [x for x in os.listdir(git_main_dirs_cwd) if ( os.path.isdir(os.path.join(git_main_dirs_cwd,x)) and x.startswith("p_") ) ]
+def run_on_all(do_py2ipynb=1, do_py_exec=1, do_ppt=1):
+    # get all git cass dirs - starting with "p_"
+    cwd = os.getcwd()
+    git_main_dirs_cwd = os.path.join(cwd, "..")
+    git_class_subdirs = [x for x in os.listdir(
+        git_main_dirs_cwd) if x.startswith("p_")]
 
-for dir_name in git_class_subdirs:
-    fp = os.path.join(git_main_dirs_cwd,dir_name)
+    for dir_name in git_class_subdirs:
+        fp = os.path.join(git_main_dirs_cwd, dir_name)
+        run_on_one_dir(fp, do_py2ipynb=do_py2ipynb,
+                       do_py_exec=do_py_exec, do_ppt=do_ppt)
 
-    print("===== IN DIR: " + fp + " =====")
+        # run on "ex#" dir
+        ex_subdirs = [x for x in os.listdir(fp) if x.startswith("ex")]
+        for ex_subdir in ex_subdirs:
+            fp = os.path.join(fp, ex_subdir)
+            # no need to execute the nb
+            run_on_one_dir(fp, do_py2ipynb=do_py2ipynb,
+                           do_py_exec=0, do_ppt=do_ppt)
+
+
+def run_on_dir_list(dir_list, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
+    for fp in dir_list:
+        run_on_one_dir(fp, do_py2ipynb=do_py2ipynb,
+                       do_py_exec=do_py_exec, do_ppt=do_ppt)
+
+
+def run_on_one_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
+
+    print("\n===== IN DIR: " + fp + " =====\n")
     for fn in os.listdir(fp):
 
         fn_no_ext = fn.split(".")[0]
 
         # make .pptx -> .pdf
-        if fn.endswith(".pptx") and DO_PPT:
-            print("converting pptx: "+ fn)
-            PPTtoPDF(os.path.join(fp,fn),os.path.join(fp,fn_no_ext+".pdf"))
+        if fn.endswith(".pptx") and do_ppt:
+            print("converting pptx: \n" + fn)
+            PPTtoPDF(os.path.join(fp, fn), os.path.join(fp, fn_no_ext+".pdf"))
 
         # make .py -> .ipynb and compile ipynb
-        elif fn.endswith(".py") and DO_PY:
-            out_ipynb = os.path.join(fp,fn_no_ext+".ipynb")
+        elif fn.endswith(".py") and do_py2ipynb:
+            out_ipynb = os.path.join(fp, fn_no_ext+".ipynb")
 
-            print("converting .py 2 .ipynb: "+ fn)
-            py2ipynb(os.path.join(fp,fn), out_ipynb)
-            
-            print("executing .ipynb : "+ fn)
-            os.system("jupyter nbconvert --ExecutePreprocessor.timeout=60 --to notebook --execute --inplace \""+out_ipynb+"\"")
+            print("converting .py 2 .ipynb: \n" + fn)
+            py2ipynb(os.path.join(fp, fn), out_ipynb)
+
+            if do_py_exec:
+                print("executing .ipynb: \n" + fn)
+                os.system(
+                    "jupyter nbconvert --ExecutePreprocessor.timeout=60 --to notebook --execute --inplace \""+out_ipynb+"\"")
+
+
+if __name__ == "__main__":
+    # run_on_dir_list(dir_list, do_py2ipynb=1, do_py_exec=1, do_ppt=1)
+    run_on_all(do_py2ipynb=1, do_py_exec=1, do_ppt=0)

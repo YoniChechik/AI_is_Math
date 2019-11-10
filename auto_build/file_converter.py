c54ef83f40f633da96ce674a25f13797563ea7bc
YoniChechik <chechik.yoni@gmail.com>
2019-11-10 18:40:40 +0200

    minor fix in zip exlude

--- auto_build/file_converter.py
+++ auto_build/file_converter.py
@@ -2,7 +2,6 @@ from py2ipynb import py2ipynb
 from ppt2pdf import PPTtoPDF
 import os
 import zipfile
-import shutil
 
 def run_on_all_classes(do_py2ipynb=1, do_py_exec=1, do_ppt=1):
     # ==== get all git cass dirs - starting with "c_"
@@ -28,11 +27,6 @@ def run_on_one_class_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
 
     print("\n===== IN DIR: " + fp + " =====\n")
 
-    # # ==== mkdir site_docs
-    # site_docs_dir = os.path.join(fp, "site_docs")
-    # if not os.path.exists(site_docs_dir):
-    #     os.mkdir(site_docs_dir)
-
     # === run on class dir data
     run_on_one_dir(fp, do_py2ipynb=do_py2ipynb, do_py_exec=do_py_exec, do_ppt=do_ppt)
 
@@ -40,11 +34,6 @@ def run_on_one_class_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
     if do_py2ipynb:
         ex_subdirs = [x for x in os.listdir(fp) if (x.startswith("ex") and os.path.isdir(os.path.join(fp,x)))]
         for ex_subdir in ex_subdirs:
-            
-            # ==== mkdir site_docs/ex
-            # site_docs_ex_dir_path = os.path.join(fp, "site_docs",ex_subdir)
-            # if not os.path.exists(site_docs_ex_dir_path):
-            #     os.mkdir(site_docs_ex_dir_path)
 
             fp_ex = os.path.join(fp, ex_subdir)
 
@@ -55,25 +44,11 @@ def run_on_one_class_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
             print("\n===== zipping ex =====\n")
             with zipfile.ZipFile(os.path.join(fp_ex,ex_subdir + '.zip'), 'w') as myzip:
                 for f in os.listdir(fp_ex):
-                    if not f.endswith(".zip") and not os.path.isdir(os.path.join(fp_ex,f)):   
+                    if not f.endswith(".zip") and f!="." and f!="..":   
                         myzip.write(os.path.join(fp_ex,f),f)
 
 
 def run_on_one_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
-    
-    # # ==== tmp copy all relevant data to site_docs_dir for executing
-    # if do_py_exec:
-    #     copied_fp_list = []
-    #     for fn in os.listdir(fp):
-    #         file_fp = os.path.join(fp, fn)
-    #         if not (file_fp.endswith(".py") or
-    #                 file_fp.endswith(".ipynb") or
-    #                 file_fp.endswith(".pdf") or
-    #                 file_fp.endswith(".pptx") or
-    #                 os.path.isdir(file_fp)):
-    #             copied_fp = os.path.join(site_docs_dir, fn)
-    #             shutil.copyfile(file_fp,copied_fp)
-    #             copied_fp_list.append(copied_fp)
 
     # ==== run on all files
     for fn in os.listdir(fp):
@@ -97,11 +72,6 @@ def run_on_one_dir(fp, do_py2ipynb=1, do_py_exec=1, do_ppt=1):
                 os.system(
                     "jupyter nbconvert --ExecutePreprocessor.timeout=60 --to notebook --execute --inplace \""+out_ipynb+"\"")
     
-    # # ==== remove all tmp copied files
-    # if do_py_exec:
-    #     for copied_fp in copied_fp_list:
-    #         os.remove(copied_fp)
-
 if __name__ == "__main__":
     dir_list = [r'C:\Users\jonathanch\Desktop\AI_is_Math\c_03_edge_detection']
     run_on_class_dir_list(dir_list, do_py2ipynb=1, do_ppt=0)

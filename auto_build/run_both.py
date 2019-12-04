075f8facb2378038e5324000a411397ab97102f1
YoniChechik <chechik.yoni@gmail.com>
2019-12-05 00:32:49 +0200

    c04 ppt change

--- auto_build/run_both.py
+++ auto_build/run_both.py
@@ -1,7 +1,7 @@
 from build_site import build_site
 from file_converter import run_on_all_classes,run_on_class_dir_list
 
-dir_list = [r'C:\Users\chech\Desktop\AI_is_Math\c_05_image_formation']
+dir_list = [r'C:\Users\chech\Desktop\AI_is_Math\c_04_curve_fitting']
 run_on_class_dir_list(dir_list, do_py2ipynb=0, do_ppt=1)
 # run_on_all_classes(do_py2ipynb=0, do_ppt=1)
 

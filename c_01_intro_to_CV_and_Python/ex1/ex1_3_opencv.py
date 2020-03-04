2b11042f7d52eb90b505e9403d8004418f149186
YoniChechik <chechik.yoni@gmail.com>
2020-03-05 00:09:07 +0200

    hint to use mask

--- c_01_intro_to_CV_and_Python/ex1/ex1_3_opencv.py
+++ c_01_intro_to_CV_and_Python/ex1/ex1_3_opencv.py
@@ -24,7 +24,7 @@ forest_img = cv2.cvtColor(forest_img,cv2.COLOR_BGR2GRAY)
 # TODO: your goal is to build the pyramids in the forest!
 #  Hints to think about:
 #  - are the images the same size?
-#  - How to mask/overlay the pixels from pyramids image to forest image
+#  - How to mask/overlay the pixels from pyramids image to forest image (use numpy masks that we've seen in the numpy notebook!!!)
 #
 #  TODO: this section can be done in 3-5 lines of nice looking code, try not to write more then 10 lines here.
 res_im = []

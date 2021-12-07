fc4927e0f373d128cc1c76d592de6d422fc8f28a
YoniChechik <chechik.yoni@gmail.com>
2021-12-07 11:46:28 +0200

    add slides about DOF + clean calibration slides

--- auto_build/BUILD.py
+++ auto_build/BUILD.py
@@ -3,21 +3,21 @@ import file_converter
 import os
 
 
-do_py2ipynb = 1
-do_ppt = 0
-is_convert_ipynb_to_html = 1
+do_py2ipynb = 0
+do_ppt = 1
+is_convert_ipynb_to_html = 0
 
 main_path = os.path.abspath("..")
 dir_list = [
     # 'c_01_intro_to_CV_and_Python',
     # 'c_02a_basic_image_processing',
     # 'c_02b_filtering_and_resampling',
-    'c_03_edge_detection',
-    # 'c_04a_curve_fitting',
+    # 'c_03_edge_detection',
+    'c_04a_curve_fitting',
     # 'c_04b_hough_transform',
-    # 'c_05_image_formation',
-    # 'c_06_geometric_transformation',
-    # 'c_07_camera_calibration',
+    'c_05_image_formation',
+    'c_06_geometric_transformation',
+    'c_07_camera_calibration',
     # 'c_08_features',
     # 'c_09_stereo',
     # 'c_10_neural_networks_basics',
